71bd380c82b0b44f8f6c155609c81de3c1457852
dongmengnan <dmn35614@ly.com>
2017-05-10 18:13:50 +0800

    add animation

--- index.js
+++ index.js
@@ -8,8 +8,22 @@ let timer;
 
 $list.css('width', $items.length * 640 + 'px');  // 动态配置父元素的宽度
 
-$('.list').on('touchstart',function(e){
+function loop(i){   // 循环向左
+	if( i < $items.length ){  
+		i++
+	}else{
+		i = 1;
+	}
+	timer = setTimeout(function() {
+		animate(i)
+		loop(i);
+	}, 2000);
+}
 
+loop(index);
+
+$('.list').on('touchstart',function(e){
+	// clearTimeout(timer);
 	startX = ( e.touches && e.touches[0] ? e.touches[0] : e ).pageX;
 
 }).on('touchmove',function(e){
@@ -17,7 +31,7 @@ $('.list').on('touchstart',function(e){
 	endX = ( e.touches && e.touches[0] ? e.touches[0] : e ).pageX;
 
 }).on('touchend',function(){
-
+	// loop(index);
 	if( endX - startX > 50 ){   // 向右滑
 		if(index > 1){
 			index--;
@@ -36,22 +50,10 @@ $('.list').on('touchstart',function(e){
 
 function animate(i){
 	$list.css({
-		'transform':`translate3d(${(i-1)*(-640)}px,0,0)`,
-		'-webkit-transform':`translate3d(${(i-1)*(-640)}px,0,0)`
-	})
+		'left':(i-1)*(-640) + 'px'
+	});
+	// $list.children("div:first").insertAfter($list.children("div:last"));
+	// $list.css("left", 0);
 	$('span.index'+i).addClass('active').siblings().removeClass('active');
 }
 
-function loop(i){   // 循环向左
-	if( i < $items.length ){  
-		i++
-	}else{
-		i = 1;
-	}
-	animate(i)
-	timer = setTimeout(function() {
-		loop(i);
-	}, 3000);
-}
-
-loop(index)
