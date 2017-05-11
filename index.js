16e2effc3062e08eabe1032f9665a11254c68a11
dongmengnan <dmn35614@ly.com>
2017-05-11 18:05:52 +0800

    增加无缝滚动，但是返回会出现空白

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let index = 1;
 let $list = $('.list'), $items = $('.item'), $dots = $('li');
 let timer;
 
-$list.css('width', $items.length * 640 + 'px');  // 动态配置父元素的宽度
+// $list.css('width', $items.length * 640 + 'px');  // 动态配置父元素的宽度
 
 function loop(i){   // 循环向左
 	if( i < $items.length ){  
@@ -20,7 +20,7 @@ function loop(i){   // 循环向左
 	}, 2000);
 }
 
-loop(index);
+// loop(index);
 
 $('.list').on('touchstart',function(e){
 	// clearTimeout(timer);
@@ -38,22 +38,62 @@ $('.list').on('touchstart',function(e){
 		}else{                 // 第一个图片位置，向右滑，回到最后一个图片位置
 			index = $items.length;
 		}
+		animateRight();
 	}else if( startX - endX > 50 ){   // 向左滑
 		if(index < $items.length){
 			index++;
 		}else{               // 最后一个位置向左滑，回到第一个图片位置
 			index = 1;
 		}
+		animateLeft();
 	}
-	animate(index);
 });
 
-function animate(i){
+let j = 0;
+let l = $items.length, 
+	w = $('.item').css('width');
+function animateLeft(){
+	j++;
 	$list.css({
-		'left':(i-1)*(-640) + 'px'
+		'left': -j*parseInt(w) + 'px'
 	});
-	// $list.children("div:first").insertAfter($list.children("div:last"));
-	// $list.css("left", 0);
-	$('span.index'+i).addClass('active').siblings().removeClass('active');
+	
+	if(j > l-1){
+		var index = j % l + 1,
+			totalLeft = -parseInt($list.css('left'));
+		$('.item'+index).css({
+			'left': totalLeft + parseInt(w) + 'px'
+		});
+		$('span.index'+index).addClass('active').siblings().removeClass('active');
+	}else{
+		$('span.index'+(j+1)).addClass('active').siblings().removeClass('active');
+	}
+	
+}
+function animateRight(){
+	j--;
+	if(j < 0 ){
+		var index = j % l == 0 ? 1 : j % l + (l+1),
+			totalLeft = parseInt($list.css('left'));
+		$('.item'+index).css({
+			'left': -(totalLeft + parseInt(w)) + 'px'
+		});
+		$('span.index'+index).addClass('active').siblings().removeClass('active');
+		$list.css({
+			'left': -j*parseInt(w) + 'px'
+		});	
+	}else{
+		console.log(j)
+		$list.css({
+			'left': -j*parseInt(w) + 'px'
+		});	
+		var index = j % l + 1,
+			totalLeft = -parseInt($list.css('left'));
+		$('.item'+index).css({
+			'left': totalLeft + parseInt(w) + 'px'
+		});
+		$('span.index'+(j+1)).addClass('active').siblings().removeClass('active');
+	}
+			
 }
 
